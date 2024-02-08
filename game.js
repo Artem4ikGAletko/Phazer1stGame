4086ab0f86d6acc87c006e8f7c588525f0875567
Artem4ikGAletko <158044271+Artem4ikGAletko@users.noreply.github.com>
2024-02-08 19:22:35 +0200

    шось підкрутив

--- game.js
+++ game.js
@@ -30,12 +30,6 @@ function preload() {
         { frameWidth: 32, frameHeight: 48 }
     );
 
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-    });
 }
 var platforms;
 
@@ -57,30 +51,30 @@ function create() {
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
 // створимо анімації
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-    });
+this.anims.create({
+    key: 'left',
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1
+});
 
-    this.anims.create({
-        key: 'turn',
-        frames: [{ key: 'dude', frame: 4 }],
-        frameRate: 20
-    });
+this.anims.create({
+    key: 'turn',
+    frames: [ { key: 'dude', frame: 4 } ],
+    frameRate: 20
+});
 
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-        frameRate: 10,
-        repeat: -1
-    });
-
-    this.physics.add.collider(player, platforms);
+this.anims.create({
+    key: 'right',
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1
+});
 // додамо курсор
-    cursors = this.input.keyboard.createCursorKeys();
+cursors = this.input.keyboard.createCursorKeys();
 
+
+    // Додали фізику зіркам
     stars = this.physics.add.group({
         key: 'star',
         repeat: 11,
@@ -92,12 +86,14 @@ function create() {
         child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
 
     });
-// додаємо фізику
+    
+
+    // рахунок
+    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+
     this.physics.add.collider(stars, platforms);
 
     this.physics.add.overlap(player, stars, collectStar, null, this);
-
-    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 
     bombs = this.physics.add.group();
 
